5d07cf90bc3d30915ee87fa871fe174d54f0fdc9
KDamsgaard <KristianDamsgaard@gmail.com>
2018-03-09 10:08:13 +0100

    Two new maps added.

---
+++ src/game2017/mapstrings/MapStrings.java
@@ -0,0 +1,82 @@
+package game2017.mapstrings;
+
+public class MapStrings {
+
+    private  String[] map1 = {    // 20x20
+            "wwwwwwwwwwwwwwwwwwww",
+            "w        ww        w",
+            "w w  w  www w  w  ww",
+            "w w  w   ww w  w  ww",
+            "w  w               w",
+            "w w w w w w w  w  ww",
+            "w w     www w  w  ww",
+            "w w     w w w  w  ww",
+            "w   w w  w  w  w   w",
+            "w     w  w  w  w   w",
+            "w ww ww        w  ww",
+            "w  w w    w    w  ww",
+            "w        ww w  w  ww",
+            "w         w w  w  ww",
+            "w        w     w  ww",
+            "w  w              ww",
+            "w  w www  w w  ww ww",
+            "w w      ww w     ww",
+            "w   w   ww  w      w",
+            "wwwwwwwwwwwwwwwwwwww"
+    };
+
+    private  String[] map2 = {    // 20x20
+            "wwwwwwwwwwwwwwwwwwww",
+            "w                  w",
+            "w  wwwwwwwww wwww  w",
+            "w  w               w",
+            "w  w www wwwwwww   w",
+            "w  w w         w   w",
+            "w  w w www ww  w   w",
+            "w    w w    w  w   w",
+            "w  w w   ww w  w   w",
+            "w  w w w ww w      w",
+            "w  w   w    w  w   w",
+            "w  w w w ww w  w   w",
+            "w  w w w  w    w   w",
+            "w  w w ww w w  w   w",
+            "w  w w      w  w   w",
+            "w  w ww wwwww  w   w",
+            "w  w           w   w",
+            "w  wwwwwww wwwww   w",
+            "w                  w",
+            "wwwwwwwwwwwwwwwwwwww"
+    };
+
+    private  String[] map3 = {    // 20x20
+            "wwwwwwwwwwwwwwwwwwww",
+            "w                  w",
+            "w                  w",
+            "w                  w",
+            "w   wwwwwwwwwww    w",
+            "w            ww    w",
+            "w           ww     w",
+            "w          ww      w",
+            "w     wwwwww       w",
+            "w       wwwww      w",
+            "w            ww    w",
+            "w             ww   w",
+            "w             ww   w",
+            "w             ww   w",
+            "w    ww     www    w",
+            "w     wwwwwww      w",
+            "w                  w",
+            "w                  w",
+            "w                  w",
+            "wwwwwwwwwwwwwwwwwwww"
+    };
+
+
+
+
+
+
+
+
+
+}
